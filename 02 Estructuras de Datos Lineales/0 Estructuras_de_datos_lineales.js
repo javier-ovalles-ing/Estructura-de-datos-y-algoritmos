03eefe0d35437b8e0e9534fb8cc953c6d695e1a0
Javier Ovalles <97541727+javier-ovalles-ing@users.noreply.github.com>
2026-08-07 17:19:23 -0400

    agregar Introduccion Estructura de Datos

---
+++ 02 Estructuras de Datos Lineales/0 Estructuras_de_datos_lineales.js
@@ -0,0 +1,6 @@
+/*
+    Las estructuras Lineales son aquellas en las que sus elementos se organizan de manera secuencial. Se utilizan cuando sus elementos deben prosesarce en orden. 
+
+    Ejemplos de estructuras lineales son: Arreglos, Listas, Pilas y Colas.
+
+*/
